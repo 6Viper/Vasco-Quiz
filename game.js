90f0948ac1c9161c08750c23f03231ed1831b866
6Viper <leofmetz@gmail.com>
2022-10-07 03:10:25 +0000

    fix ppoint

--- game.js
+++ game.js
@@ -7,7 +7,6 @@ const scoreText = document.querySelector('#score');
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
-let incorrect = 0
 let questionCounter = 0
 let availableQuestions = []
 
@@ -48,20 +47,19 @@ let questions = [
 ]
 
 const SCORE_POINTS = 1
-const INCORRECT_POINTS = 1
+const SCORE2_POINTS = -1
 const MAX_QUESTIONS = 5
 
 startGame = function() {
     questionCounter = 0
     score = 0
-    incorrect = 0
     availableQuestions = [...questions]
     getNewQuestion()
 }
 
 getNewQuestion = function() {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score, incorrect)
+        localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
     }
@@ -92,10 +90,12 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
+  
         if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
-        } 
+        } else if(classToApply === 'incorrect') {
+            incrementScore(SCORE2_POINTS)
+        }    
 
         selectedChoice.parentElement.classList.add(classToApply)
 
@@ -113,3 +113,4 @@ incrementScore = num => {
 }
 
 startGame()
+
